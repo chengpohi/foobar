5a4fa483ee369a7f8d9fe4f9c60a35dc6a5f2ee0
Xiang Chen <chengpohi@gmail.com>
2015-10-22 23:20:05 +0800

    DONE:scala 99 --- TODO:

--- src/main/scala/Scala99.scala
+++ src/main/scala/Scala99.scala
@@ -1,59 +1,111 @@
+import scala.collection.mutable
+import scala.util.Random
+
 /**
  * scala99
  * Created by chengpohi on 10/7/15.
  */
 class Scala99 {
-  def lastOne[T](arrs: List[T]): T = {
-    arrs.last
+  def lastOne[T](list: List[T]): T = {
+    list.last
   }
 
-  def kTh[T](arrs: List[T], k: Int): T = {
-    arrs(k - 1)
+  def kTh[T](list: List[T], k: Int): T = {
+    list(k - 1)
   }
 
-  def reverse[T](arrs: List[T]): List[T] = arrs match {
+  def reverse[T](list: List[T]): List[T] = list match {
     case al :+ a0 => a0 :: reverse(al)
     case Nil => Nil
   }
 
-  def palindrome[T](arrs: List[T]): Boolean = arrs match {
+  def palindrome[T](list: List[T]): Boolean = list match {
     case List(a) => true
     case head :: ar => head == ar.last && palindrome(ar.init)
     case List() => true
     case _ => false
   }
 
-  def flatten[T](arrs: List[T]): List[Any] = arrs flatMap {
+  def flatten[T](list: List[T]): List[Any] = list flatMap {
     case a: List[_] => flatten(a)
     case b => List(b)
   }
 
-  def compress[T](arrs: List[T]): List[T] = arrs.distinct
+  def compress[T](list: List[T]): List[T] = list.distinct
 
-  def pack[T](arrs: List[T]): List[List[T]] = {
-    arrs.groupBy(a => a).values.toList
+  def pack[T](list: List[T]): List[List[T]] = {
+    list.groupBy(a => a).values.toList
   }
 
-  def encode[T](arrs: List[T]): List[(Int, T)] = {
-    arrs.groupBy(a => a).toList.map(f => (f._2.size, f._1))
+  def encode[T](list: List[T]): List[(Int, T)] = {
+    list.groupBy(a => a).toList.map(f => (f._2.size, f._1))
   }
 
-  def encodeModified[T](arrs: List[T]): List[Any] = {
-    arrs.groupBy(a => a).toList.map {
+  def encodeModified[T](list: List[T]): List[Any] = {
+    list.groupBy(a => a).toList.map {
       case f if f._2.size == 1 => f._1
       case f => (f._2.size, f._1)
     }
   }
 
-  def duplicate[T](arrs: List[T]): List[T] = {
-    arrs.groupBy(a => a).toList.flatMap(f => (for (i <- 1 to f._2.size * 2) yield f._1).toList)
+  def duplicate[T](list: List[T]): List[T] = {
+    list.groupBy(a => a).toList.flatMap(f => (for (i <- 1 to f._2.size * 2) yield f._1).toList)
   }
 
-  def duplicate[T](arrs: List[T], n: Int): List[T] = {
-    arrs.groupBy(a => a).toList.flatMap(f => (for (i <- 1 to f._2.size * n) yield f._1).toList)
+  def duplicate[T](list: List[T], n: Int): List[T] = {
+    list.groupBy(a => a).toList.flatMap(f => (for (i <- 1 to f._2.size * n) yield f._1).toList)
   }
 
-  def decodeModified[T](arrs: List[(Int, T)]): List[T] = {
-    arrs.flatMap(f => (for (i <- 1 to f._1) yield f._2).toList)
+  def remove[T](list: List[T], i: Int): List[T] = {
+    list.zipWithIndex.filter(n => (n._2 + 1) % i != 0).map(f => f._1)
+  }
+
+  def split[T](list: List[T], i: Int): (List[T], List[T]) = {
+    list.splitAt(i)
+  }
+
+  def rotate[T](list: List[T], i: Int): List[T] = {
+    val l: (List[T], List[T]) = list.splitAt(i)
+    l._2 ::: l._1
+  }
+
+  def removeByIndex[T](list: List[T], i: Int): List[T] = {
+    list.patch(i - 1, Nil, 1)
+  }
+
+  def insert[T](list: List[T], a: T, i: Int): List[T] = {
+    val l: (List[T], List[T]) = list.splitAt(i)
+    (l._1 :+ a) ::: l._2
+  }
+
+  def randSelect[T](list: List[T], a: Int): List[T] = {
+    val s = new mutable.LinkedHashSet[Int]()
+    val max = list.size
+    while (s.size < a) {
+      val nextInt: Int = Random.nextInt(max)
+      if (!s.contains(nextInt)) {
+        s.add(nextInt)
+      }
+    }
+    s.toList.map(i => list(i))
+  }
+
+  def randSelectDiff(a: Int, max: Int): List[Int] = {
+    val s = new mutable.LinkedHashSet[Int]()
+    while (s.size < a) {
+      val nextInt: Int = Random.nextInt(max)
+      if (!s.contains(nextInt)) {
+        s.add(nextInt)
+      }
+    }
+    s.toList
+  }
+
+  def randPermu[T](list: List[T]): List[T] = {
+    randSelect(List.range(0, list.size), list.size).map(i => list(i))
+  }
+
+  def decodeModified[T](list: List[(Int, T)]): List[T] = {
+    list.flatMap(f => (for (i <- 1 to f._1) yield f._2).toList)
   }
 }
--- src/test/scala/Scala99Test.scala
+++ src/test/scala/Scala99Test.scala
@@ -25,8 +25,16 @@ class Scala99Test extends FlatSpec with ShouldMatchers {
     scala99 encode List("1", "2", "2", "3", "3") should equal(List((2, "2"), (1, "1"), (2, "3")))
     scala99 encodeModified List(1, 2, 2, 3, 3) should equal(List((2, 2), 1, (2, 3)))
     scala99 decodeModified List((2, 2), (1, 1), (2, 3)) should equal(List(2, 2, 1, 3, 3))
-    scala99 duplicate  List(1, 2, 2, 3, 3) should equal(List(2, 2, 2, 2, 1, 1, 3, 3, 3, 3))
+    scala99 duplicate List(1, 2, 2, 3, 3) should equal(List(2, 2, 2, 2, 1, 1, 3, 3, 3, 3))
     scala99 duplicate(List(1, 2, 2, 3, 3), 3) should equal(List(2, 2, 2, 2, 2, 2, 1, 1, 1, 3, 3, 3, 3, 3, 3))
+    scala99 remove(List(1, 2, 2, 3, 3, 4), 3) should equal(List(1, 2, 3, 3))
+    scala99 split(List(1, 2, 2, 5, 3, 4), 3) should equal((List(1, 2, 2), List(5, 3, 4)))
+    scala99 rotate(List(1, 2, 3, 4, 5), 3) should equal(List(4, 5, 1, 2, 3))
+    scala99 removeByIndex(List(1, 2, 3, 4, 5), 3) should equal(List(1, 2, 4, 5))
+    scala99 insert(List(1, 2, 3, 4, 5), 8, 3) should equal(List(1, 2, 3, 8, 4, 5))
+    numbers should contain((scala99 randSelect(numbers, 1)).head)
 
+    (scala99 randSelectDiff(5, 20)).size should equal(5)
+    (scala99 randPermu List(1, 2, 3, 4, 5)).distinct.size should equal(5)
   }
 }
