6ce72b4a5b8111e2dd24b960ee5ac64e3ba1e6d5
Xiang Chen <chengpohi@gmail.com>
2015-11-01 18:48:58 +0800

    DONE:truth table --- TODO:

---
+++ src/main/scala/TruthTable.scala
@@ -0,0 +1,24 @@
+
+/**
+ * scala99
+ * Created by chengpohi on 11/1/15.
+ */
+object TruthTable {
+  def and(a: Boolean, b: Boolean): Boolean = a && b
+
+  def or(a: Boolean, b: Boolean): Boolean = a || b
+
+  def not(a: Boolean): Boolean = !a
+
+  def equ(a: Boolean, b: Boolean): Boolean = or(and(a, b), and(not(a), not(b)))
+
+  def table2(f: (Boolean, Boolean) => Boolean): List[(Boolean, Boolean, Boolean)] = {
+    val two = List(true, false)
+    for (a <- two; b <- two) yield (a, b, f(a, b))
+  }
+
+  def table3(f: (Boolean, Boolean, Boolean) => Boolean): List[(Boolean, Boolean, Boolean, Boolean)] = {
+    val two = List(true, false)
+    for (a <- two; b <- two; c <- two) yield (a, b, c, f(a, b, c))
+  }
+}
---
+++ src/test/scala/TruthTableTest.scala
@@ -0,0 +1,14 @@
+import org.scalatest.{ShouldMatchers, FlatSpec}
+import TruthTable._
+
+/**
+ * scala99
+ * Created by chengpohi on 11/1/15.
+ */
+class TruthTableTest extends FlatSpec with ShouldMatchers {
+  it should "" in {
+    table2((a: Boolean, b: Boolean) => and(a, or(a, b))) should equal(List((true, true, true), (true, false, true), (false, true, false), (false, false, false)))
+    table2((a: Boolean, b: Boolean) => and(a, or(a, !b))) should equal(List((true, true, true), (true, false, true), (false, true, false), (false, false, false)))
+    table3((a: Boolean, b: Boolean, c: Boolean) => and(or(and(equ(and(a, or(b, c)), a), b), a), c)) should equal(List((true, true, true, true), (true, true, false, false), (true, false, true, true), (true, false, false, false), (false, true, true, true), (false, true, false, false), (false, false, true, false), (false, false, false, false)))
+  }
+}
