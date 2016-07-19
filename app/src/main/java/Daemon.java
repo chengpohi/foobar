2d56099bfa3dccc0072b8da75428bb9700e91e0d
Xiang Chen <chengpohi@gmail.com>
2016-07-19 22:33:41 +0800

    DONE:java daemon

---
+++ app/src/main/java/Daemon.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * scala99
+ * Created by chengpohi on 7/19/16.
+ */
+public class Daemon {
+    private final CountDownLatch keepAliveLatch = new CountDownLatch(1);
+    private final Thread keepAliveThread;
+
+    public Daemon() {
+        keepAliveThread = new Thread(() -> {
+            try {
+                keepAliveLatch.await();
+                System.out.println("Exit !!!");
+            } catch (InterruptedException e) {
+            }
+        });
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void start() {
+                keepAliveLatch.countDown();
+                System.out.println("Shutdown Hook Executed !!!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Daemon daemon = new Daemon();
+        daemon.start();
+        System.out.println("Started !!!");
+    }
+
+    public void start() {
+        keepAliveThread.start();
+    }
+}
+
+
