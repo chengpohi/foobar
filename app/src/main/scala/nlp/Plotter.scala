233b22f407fa52a8ecbdec78e05f7265dc87517b
Xiang Chen <chengpohi@gmail.com>
2016-07-26 00:10:45 +0800

    DONE:log base

--- app/src/main/scala/nlp/Plotter.scala
+++ app/src/main/scala/nlp/Plotter.scala
@@ -2,17 +2,23 @@ package nlp
 
 import breeze.linalg._
 import breeze.plot._
+import breeze.numerics._
+
 /**
   * scala99
   * Created by chengpohi on 7/9/16.
   */
 object Plotter {
-  def main(args: Array[String]) {
-    val f = Figure()
+  def main(args: Array[String]): Unit = {
+    logPlot(2)
+    logPlot(10)
+  }
+
+  def logPlot(base: Int) = {
+    val f = Figure("Base: " + base)
     val p = f.subplot(0)
-    val x = linspace(0.0,1.0)
-    p += plot(x, x :^ 2.0)
-    p += plot(x, x :^ 3.0, '.')
+    val x = linspace(1.0, 10.0, 100)
+    p += plot(x, log(x) / log(base))
     p.xlabel = "x axis"
     p.ylabel = "y axis"
     f.saveas("imgs/lines.png")
