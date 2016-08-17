1d3af3240f3a9c696da3856161973974c0c403d8
Xiang Chen <chengpohi@gmail.com>
2016-08-18 00:15:10 +0800

    DONE:prime factors

---
+++ app/src/main/scala/algorithm/OJ.scala
@@ -0,0 +1,30 @@
+package algorithm
+
+/**
+  * scala99
+  * Created by chengpohi on 8/17/16.
+  */
+object OJ {
+  def primeStream(s: Stream[Int]): Stream[BigInt] = s.head #:: primeStream(s.tail filter {
+    _ % s.head != 0
+  })
+  val primes = primeStream(Stream.from(2))
+
+  def primeFactors(r: Stream[BigInt], n: BigInt): Stream[BigInt] = {
+    if (r.head >= n)
+      return Stream.empty
+    val f: Option[BigInt] = r.find(i => n % i == 0)
+    f match {
+      case Some(t) => t #:: primeFactors(r.tail, n / t)
+      case None => Stream.empty
+    }
+  }
+
+  val pf = (n: BigInt) => primeFactors(primes, n)
+
+  def main(args: Array[String]): Unit = {
+    val n: BigInt = BigInt("600851475143")
+    val result: Stream[BigInt] = pf(n)
+    println(result.last)
+  }
+}
