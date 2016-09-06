5d45350ed2798eb5b97c51edf9a56bce716a78ee
Xiang Chen <chengpohi@gmail.com>
2016-09-06 21:35:52 +0800

    DONE:bfs

---
+++ app/src/test/scala/algorithm/SearchTest.scala
@@ -0,0 +1,23 @@
+package algorithm
+
+import breeze.linalg.DenseMatrix
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+/**
+  * scala99
+  * Created by chengpohi on 9/6/16.
+  */
+class SearchTest extends FlatSpec with ShouldMatchers {
+  it should "list routes two nodes by bfs" in {
+    val matrix = DenseMatrix(
+      (0, 0, 0),
+      (0, 1, 0),
+      (0, 0, 0)
+    )
+    import MatrixOps.BFS
+    val start = (0, 0)
+    val end = (2, 2)
+    val result: Option[List[List[(Int, Int)]]] = matrix.routes(start, end)
+    assert(result === Some(List(List((0, 0), (0, 1), (0, 2), (1, 2), (2, 2)), List((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)))))
+  }
+}
