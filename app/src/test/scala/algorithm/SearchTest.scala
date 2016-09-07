8ede951fdb2c06fe5bbeb0fd63bac43b9aab598a
Xiang Chen <chengpohi@gmail.com>
2016-09-07 23:45:50 +0800

    DONE:adjancy matrix

--- app/src/test/scala/algorithm/SearchTest.scala
+++ app/src/test/scala/algorithm/SearchTest.scala
@@ -21,7 +21,12 @@ class SearchTest extends FlatSpec with ShouldMatchers {
     assert(result === Some(List(List((0, 0), (0, 1), (0, 2), (1, 2), (2, 2)), List((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)))))
   }
   it should "list routes two nodes by dfs" in {
-    val matrix = DenseMatrix.zeros[Int](3, 4)
+    val matrix = DenseMatrix(
+      (2, 1, 1, 0), // (0, 0), (0, 1), (0, 2)
+      (1, 0, 0, 1), // (1, 0), (1, 3)
+      (1, 0, 0, 1), // (2, 0), (2, 3)
+      (0, 1, 1, 0)  // (3, 1), (3, 2)
+    )
     import MatrixOps.BFS
     val start = (0, 0) // root node
     matrix.route
