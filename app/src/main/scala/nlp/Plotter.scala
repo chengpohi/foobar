9beaba40467006167384333acbf4bb7e02ebad88
Xiang Chen <chengpohi@gmail.com>
2016-09-13 19:54:53 +0800

    DONE:bad viz, hardly to expand and write dsl for it, rewrite ???

--- app/src/main/scala/nlp/Plotter.scala
+++ app/src/main/scala/nlp/Plotter.scala
@@ -1,26 +1,73 @@
 package nlp
 
 import breeze.linalg._
-import breeze.plot._
 import breeze.numerics._
+import breeze.plot._
+import nlp.FigureDSL.PlotBuilder
 
 /**
   * scala99
   * Created by chengpohi on 7/9/16.
   */
-object Plotter {
-  def main(args: Array[String]): Unit = {
-    logPlot(2)
-    logPlot(10)
+object FigureDSL {
+  case class LinePlotDefinition[D](var x: D, var y: D)
+  class PlotBuilder(implicit visible: Boolean = true) {
+    val f = Figure()
+    f.visible = visible
+    val p = f.subplot(0)
+    def display = {
+      f.refresh()
+      this
+    }
   }
 
-  def logPlot(base: Int) = {
-    val f = Figure("Base: " + base)
-    val p = f.subplot(0)
-    val x = linspace(0.1, 10.0, 100)
-    p += plot(x, log(x) / log(base))
-    p.xlabel = "x axis"
-    p.ylabel = "y axis"
-    f.saveas("imgs/lines.png")
+  case class HistPlotBuilder[D, V](data: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean)
+    extends PlotBuilder {
+    def bins(b: Int) = {
+      p += breeze.plot.hist(data, b)
+      this
+    }
+
+    def title(t: String) = {
+      p.title = t
+      this
+    }
+  }
+
+  case class LinePlotBuilder[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) extends PlotBuilder {
+    def Y(y: D) = {
+      p += plot(x, y)
+      this
+    }
+  }
+
+  case object hist {
+    def data[D, V](d: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) = HistPlotBuilder(d)
+  }
+
+  case object line {
+    def X[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) = LinePlotBuilder(x)
+  }
+}
+
+object FigurePlotter {
+  def apply(plotBuilder: PlotBuilder) = {
+    plotBuilder.display
+  }
+}
+
+object Plotter {
+  def main(args: Array[String]): Unit = {
+    import FigureDSL._
+    implicit val visible: Boolean = true
+    FigurePlotter {
+      val g = breeze.stats.distributions.Gaussian(0, 1).sample(100000)
+      hist data g bins 100 title "Hello World"
+    }
+
+    FigurePlotter {
+      val x = linspace(0.1, 10.0, 100)
+      line X x Y log(x)
+    }
   }
 }
