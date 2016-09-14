b55dc2cb94195ab70c9037b91e09b4494cfaeff4
Xiang Chen <chengpohi@gmail.com>
2016-09-14 22:46:16 +0800

    DONE:figure

--- app/src/main/scala/nlp/Plotter.scala
+++ app/src/main/scala/nlp/Plotter.scala
@@ -5,53 +5,61 @@ import breeze.numerics._
 import breeze.plot._
 import nlp.FigureDSL.PlotBuilder
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * scala99
   * Created by chengpohi on 7/9/16.
   */
 object FigureDSL {
   case class LinePlotDefinition[D](var x: D, var y: D)
-  class PlotBuilder(implicit visible: Boolean = true) {
-    val f = Figure()
-    f.visible = visible
-    val p = f.subplot(0)
-    def display = {
+  class PlotBuilder {
+    var series = new ArrayBuffer[Series]
+    var ti = ""
+
+    def title(t: String) = {
+      ti = t
+      this
+    }
+
+    def display(implicit visible: Boolean = true) = {
+      val f = Figure()
+      f.visible = visible
+      val p = f.subplot(0)
+      p ++= series
+      p.title = ti
       f.refresh()
       this
     }
   }
 
-  case class HistPlotBuilder[D, V](data: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean)
+  case class HistPlotBuilder[D, V](data: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double)
     extends PlotBuilder {
     def bins(b: Int) = {
-      p += breeze.plot.hist(data, b)
+      series += breeze.plot.hist(data, b)
       this
     }
 
-    def title(t: String) = {
-      p.title = t
-      this
-    }
   }
 
-  case class LinePlotBuilder[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) extends PlotBuilder {
+  case class LinePlotBuilder[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double) extends PlotBuilder {
     def Y(y: D) = {
-      p += plot(x, y)
+      series += plot(x, y)
       this
     }
   }
 
   case object hist {
-    def data[D, V](d: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) = HistPlotBuilder(d)
+    def data[D, V](d: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double) = HistPlotBuilder(d)
   }
 
   case object line {
-    def X[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double, visible: Boolean) = LinePlotBuilder(x)
+    def X[D, V](x: D)(implicit xv: DomainFunction[D, Int, V], vv: V => Double) = LinePlotBuilder(x)
   }
 }
 
 object FigurePlotter {
-  def apply(plotBuilder: PlotBuilder) = {
+  def apply(plotBuilder: PlotBuilder)(implicit visible: Boolean) = {
     plotBuilder.display
   }
 }
