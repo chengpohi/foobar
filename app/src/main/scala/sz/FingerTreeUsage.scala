6d0cca55ab93e9a3120832574565293ce871396d
Xiang Chen <chengpohi@gmail.com>
2017-04-21 18:33:34 +0800

    DONE:FoldableUsage

---
+++ app/src/main/scala/sz/FingerTreeUsage.scala
@@ -0,0 +1,49 @@
+package sz
+
+import collection.immutable.Stream
+import scalaz.{FingerTree, Reducer, UnitReducer}
+
+object FingerTreeUsage extends App {
+
+  import scalaz.FingerTree._
+  import scalaz.std.anyVal._
+
+  val intStream = Stream.from(1)
+
+  implicit def SizeReducer[A]: Reducer[A, Int] = UnitReducer(x => 1)
+
+  def streamToTree[A](stream: Stream[A]): FingerTree[Int, A] = stream.foldLeft(empty(SizeReducer[A])) {
+    case (t, x) => (t :+ x)
+  }
+
+  val emptyTree = empty[Int, Int](SizeReducer[Int])
+  assert(emptyTree.isEmpty)
+
+  //prepending
+  assert((2 +: 3 +: 4 +: emptyTree).toList == List(2, 3, 4))
+
+  //appending
+  assert((emptyTree :+ 2 :+ 3 :+ 4).toList == List(2, 3, 4))
+
+  //folding
+  assert(streamToTree(intStream.take(20)).foldRight(0)(_ + _) == (1 to 20).sum)
+
+  //replace the first element of the tree
+  assert((5 |-: streamToTree(intStream.take(3))).toList == List(5, 2, 3))
+
+  //replace the last element of the tree
+  assert((streamToTree(intStream.take(3)) :-| 5).toList == List(1, 2, 5))
+
+  //appending two trees
+  assert((streamToTree(intStream.take(5)) <++> streamToTree(Stream.from(6).take(5))).toStream == intStream.take(10))
+
+
+  import scalaz.std.option._
+
+  //traversing the tree
+  val traversedTree = streamToTree(intStream.take(10)).traverseTree[Option, Int, Int](i => Some(i * 2))
+  assert(traversedTree.map(_.toStream).getOrElse(Stream.empty) == intStream.map(_ * 2).take(10))
+
+  println(traversedTree.toString)
+
+}
---
+++ app/src/main/scala/sz/Foldable1Usage.scala
@@ -0,0 +1,49 @@
+package sz
+
+import scalaz.{Foldable1, IList, NonEmptyList, OneAnd}
+import scalaz.syntax.foldable1._
+import scalaz.syntax.equal._
+import scalaz.std.anyVal._
+import scalaz.std.option._
+import scalaz.std.string._
+
+object Foldable1Usage extends App {
+  assert(Foldable1[NonEmptyList].foldMap1(NonEmptyList(1, 2, 3))(identity) === 6)
+  assert(Foldable1[NonEmptyList].foldMap1(NonEmptyList("1", "2", "3"))(identity) === "123")
+  assert(NonEmptyList(1, 2, 3).foldMap1(identity) === 6)
+  assert(NonEmptyList("1", "2", "3").foldMap1(identity) === "123")
+
+  assert(NonEmptyList(1, 2, 3).foldMap1(identity) === 6)
+  assert(NonEmptyList("1", "2", "3").foldMap1(identity) === "123")
+
+  // or with a slightly less trivial function:
+  assert(Foldable1[NonEmptyList].foldMap1(NonEmptyList(1, 2, 3))(_.toString) === "123")
+  assert(NonEmptyList(1, 2, 3).foldMap1(_.toString) === "123")
+
+  sealed trait Tree[A]
+
+  sealed case class N[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  sealed case class L[A](a: A) extends Tree[A]
+
+  assert(NonEmptyList(1, 2, 3, 4).foldMapRight1[Tree[Int]](L.apply)((a, b) ⇒ N(L(a), b)) == N(L(1), N(L(2), N(L(3), L(4)))))
+  assert(NonEmptyList(1, 2, 3, 4).minimum === Some(1))
+  assert(Foldable1[NonEmptyList].minimum1(NonEmptyList(1, 2, 3, 4)) === 1)
+  assert(NonEmptyList(1, 2, 3, 4).minimum1 === 1)
+
+  assert(NonEmptyList(1, 2, 3, 4).maximum === Some(4))
+  assert(Foldable1[NonEmptyList].maximum1(NonEmptyList(1, 2, 3, 4)) === 4)
+  assert(NonEmptyList(1, 2, 3, 4).maximum1 === 4)
+
+
+  assert(NonEmptyList("a", "aa", "aaa").minimumBy(_.length) === Some("a"))
+  assert(Foldable1[NonEmptyList].minimumBy1(NonEmptyList("a", "aa", "aaa"))(_.length) === "a")
+  assert(NonEmptyList("a", "aa", "aaa").minimumBy1(_.length) === "a")
+
+  assert(NonEmptyList("a", "aa", "aaa").maximumBy(_.length) === Some("aaa"))
+  assert(Foldable1[NonEmptyList].maximumBy1(NonEmptyList("a", "aa", "aaa"))(_.length) === "aaa")
+  assert(NonEmptyList("a", "aa", "aaa").maximumBy1(_.length) === "aaa")
+
+
+  assert(OneAnd(1, IList(2,3)).foldMap1(identity) === 6)
+}
