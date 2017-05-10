0f9c473d5cb368aa2fe509435c5c7a294879406e
chengpohi <chengpohi@gmail.com>
2017-05-10 23:45:19 +0800

    DONE:ID usage

---
+++ app/src/main/scala/sz/IdUsage.scala
@@ -0,0 +1,16 @@
+package sz
+
+object IdUsage extends App {
+
+  import scalaz._
+  import Scalaz._
+
+  def f(a: String): Int = a.toInt
+
+  def g(i: Int): Double = i.toDouble
+
+
+  val r = "1" |> f |> g
+
+  println(r)
+}
