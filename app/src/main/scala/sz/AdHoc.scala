78f165125eeaf1331d1ff53e25e218065c79c5cc
chengpohi <chengpohi@gmail.com>
2017-11-05 22:08:15 +0800

    [XiangChen]:AdHoc Example

--- app/src/main/scala/sz/AdHoc.scala
+++ app/src/main/scala/sz/AdHoc.scala
@@ -11,13 +11,35 @@ trait Plus[A] {
 }
 
 object AdHoc {
+
   implicit object PlusString extends Plus[String] {
     override def plus(a1: String, a2: String): String = s"""{"$a1": "$a2"}"""
   }
+
   implicit object PlusInt extends Plus[Int] {
     override def plus(a1: Int, a2: Int): Int = a1 + a2
   }
+
   implicit object PlusUser extends Plus[User] {
     override def plus(a1: User, a2: User): User = ???
   }
+
+}
+
+object PlusUsage extends App {
+  trait PlusOps[T] {
+    val p: Plus[T]
+    val v: T
+    def plus(t: T): T = {
+      p.plus(v, t)
+    }
+  }
+
+  implicit def toPlus[T: Plus](f: T)(implicit ps: Plus[T]) = new PlusOps[T] {
+    override val p = ps
+    override val v = f
+  }
+  import AdHoc._
+  val res = "hello".plus("world")
+  println(res)
 }
