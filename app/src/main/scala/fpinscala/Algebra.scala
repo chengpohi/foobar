a1a74a22cbecd8ce6cc069b5a62f04aafd8d3dc1
Xiang.Chen <chengpohi@gmail.com>
2017-11-22 00:50:20 +0800

    [XiangChen]:IO run

--- app/src/main/scala/fpinscala/Algebra.scala
+++ app/src/main/scala/fpinscala/Algebra.scala
@@ -1,5 +1,6 @@
 package fpinscala
 
+
 object Algebra extends App {
 
   trait Monoid[A] {
@@ -74,4 +75,142 @@ object Algebra extends App {
     def map[A, B](as: List[A])(f: A => B): List[B] = as map f
 
   }
+
+  case class Player(name: String, score: Int)
+
+  sealed trait IO[A] {
+    def flatMap[B](f: A => IO[B]): IO[B] =
+      FlatMap(this, f)
+    def map[B](f: A => B): IO[B] =
+      flatMap(f andThen (Return(_)))
+  }
+
+  case class Return[A](a: A) extends IO[A]
+
+  case class Suspend[A](resume: () => A) extends IO[A]
+
+  case class FlatMap[A, B](sub: IO[A], k: A => IO[B]) extends IO[B]
+
+  object IO extends Monad[IO] {
+    override def unit[A](a: => A): IO[A] = new IO[A] {
+      def run = a
+    }
+    override def flatMap[A, B](fa: IO[A])(f: A => IO[B]) = fa flatMap f
+    def apply[A](a: => A): IO[A] = unit(a)
+  }
+
+  def fahrenheitToCelsius(f: Double): Double = (f - 32) * 5.0 / 9.0
+
+  def winner(p1: Player, p2: Player): Option[Player] = {
+    if (p1.score > p2.score) Some(p1)
+    else if (p1.score < p2.score) Some(p2)
+    else None
+  }
+
+  def winnerMsg(p: Option[Player]): String = p map { case Player(name, _) => s"$name is the winner!"
+  } getOrElse "It's a draw."
+
+  def contest(p1: Player, p2: Player): IO[Unit] =
+    printLine(winnerMsg(winner(p1, p2)))
+
+  def ReadLine: IO[String] = IO {
+    scala.io.StdIn.readLine()
+  }
+
+  def printLine(msg: String): IO[Unit] = IO {
+    Suspend(() => println(msg))
+  }
+
+  def converter: IO[Unit] = for {
+    _ <- printLine("Enter a temperature in degrees Fahrenheit: ")
+    d <- ReadLine.map(_.toDouble)
+    _ <- printLine(fahrenheitToCelsius(d).toString)
+  } yield ()
+
+  @annotation.tailrec def run[A](io: IO[A]): A = io match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x, f) => x match {
+      case Return(a) => run(f(a))
+      case Suspend(r) => run(f(r()))
+      case FlatMap(y, g) => run(y flatMap (a => g(a) flatMap f))
+    }
+  }
+
+  val f: Int => IO[Int] = (x: Int) => Return(x)
+
+  val g = List.fill(100000)(f).foldLeft(f) {
+    (a, b) =>
+      x: Int =>
+        Suspend(() => ()).flatMap { _ => a(x).flatMap(b) }
+  }
+  run(g(42))
+}
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(_._1), map(fab)(_._2))
+
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
+}
+
+object Functor {
+  val listFunctor = new Functor[List] {
+    def map[A, B](as: List[A])(f: A => B): List[B] = as map f
+  }
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    join(map(ma)(f))
+
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+
+  def sequence[A](lma: List[F[A]]): F[List[A]] =
+    lma.foldRight(unit(List[A]()))((ma, mla) => map2(ma, mla)(_ :: _))
+
+  def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+    la.foldRight(unit(List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
+
+  // For `List`, the `replicateM` function will generate a list of lists.
+  // It will contain all the lists of length `n` with elements selected from the
+  // input list.
+  // For `Option`, it will generate either `Some` or `None` based on whether the
+  // input is `Some` or `None`. The `Some` case will contain a list of length `n`
+  // that repeats the element in the input `Option`.
+  // The general meaning of `replicateM` is described very well by the
+  // implementation `sequence(List.fill(n)(ma))`. It repeats the `ma` monadic value
+  // `n` times and gathers the results in a single value, where the monad `M`
+  // determines how values are actually combined.
+
+  // Recursive version:
+  def _replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    if (n <= 0) unit(List[A]()) else map2(ma, _replicateM(n - 1, ma))(_ :: _)
+
+  // Using `sequence` and the `List.fill` function of the standard library:
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    sequence(List.fill(n)(ma))
+
+
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  def _flatMap[A, B](ma: F[A])(f: A => F[B]): F[B] =
+    compose((_: Unit) => ma, f)(())
+
+  def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
+
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
+    ms.foldRight(unit(List[A]()))((x, y) =>
+      compose(f, (b: Boolean) => if (b) map2(unit(x), y)(_ :: _) else y)(x))
 }
