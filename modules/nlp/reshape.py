cec51dda176f2525eb1037917d247ac6916f9f9a
chengpohi <chengpohi@gmail.com>
2018-01-24 23:46:32 +0800

    [TECH] - range

--- modules/nlp/reshape.py
+++ modules/nlp/reshape.py
@@ -2,31 +2,31 @@ import tensorflow as tf
 
 sess = tf.InteractiveSession()
 
-# 3 * 3
+print("-----" * 5)
 print(tf.reshape(list(range(0, 9)), [3, 3]).eval())
 
-print("-----" * 20)
-print(tf.reshape(list(range(0, 9)), [3, 3]).eval())
-
-print("-----" * 20)
+print("-----" * 5)
 r = tf.reshape(list(range(0, 9)), [3, 3]).eval()
 
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(r, [-1]).eval())
 
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(r, [3, 3]).eval())
 
-print("-----" * 20)
+print("-----" * 5)
+print(tf.reshape(r, [9]).eval())
+
+print("-----" * 5)
 print(tf.reshape(list(range(0, 18)), [2, 3, 3]).eval())
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(list(range(0, 18)), [2, 3, 3, 1]).eval())
 
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(list(range(0, 18)), [1, 3, 3, 2]).eval())
 
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(list(range(0, 18)), [2, 1, 3, 3]).eval())
 
-print("-----" * 20)
+print("-----" * 5)
 print(tf.reshape(list(range(0, 18)), [2, 3, 1, 3]).eval())
