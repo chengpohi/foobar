52f9b8a301fab103de91eaf0b2dd83e8a63d978d
chengpohi <chengpohi@gmail.com>
2018-03-23 15:58:49 +0800

    [] - model home

--- modules/nlp/nmt/nmt_train.py
+++ modules/nlp/nmt/nmt_train.py
@@ -76,8 +76,8 @@ def main(unused_argv):
     FLAGS.num_units = 128
     FLAGS.dropout = 0.2
     FLAGS.metrics = "bleu"
-    FLAGS.batch_size = 2
-    FLAGS.tgt_max_len_infer = 1
+    # FLAGS.batch_size = 2
+    # FLAGS.tgt_max_len_infer = 1
 
     default_hparams = create_hparams(FLAGS)
     train_fn = train.train
