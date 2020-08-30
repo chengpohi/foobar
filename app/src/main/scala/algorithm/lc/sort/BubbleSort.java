8bdf15ffee0b3fcb9ea9d829272c8ec8481f42f5
Xiang.Chen <yiyou.cx@alibaba-inc.com>
2020-08-30 16:05:44 +0800

    foobar - Xiang.Chen - fix bubble sort

--- app/src/main/scala/algorithm/lc/sort/BubbleSort.java
+++ app/src/main/scala/algorithm/lc/sort/BubbleSort.java
@@ -7,13 +7,12 @@ public class BubbleSort {
         for (int i = 0; i <arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j + 1] < arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-
     }
 
     public static void main(String[] args) {
